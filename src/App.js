b0b8cae8f25d0ee2a92d7b38add0d0944d018e67
Ronald Estupiñan <ronaldestupinan@ZCOBTMH100224-RonaldEstupinan.local>
2019-10-07 17:05:56 -0500

    fixing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  
+  handleChange (e) {
+    var lenghtChar = e.target.value.length
+    this.setState({count:lenghtChar})
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange= {this.handleChange}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
